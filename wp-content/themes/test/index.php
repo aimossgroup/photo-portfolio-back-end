bbe482b85522760c8567d2f8749cd69255584f9a
yellowcoma <gthtcnfym12git>
2017-02-21 16:25:17 +0300

    new branch + test(clear theme)

---
+++ wp-content/themes/test/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> >
+<head>
+	<title><?php echo wp_get_document_title(); ?></title>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+	<!--[if lt IE 9]>
+	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
+	<![endif]-->
+	<!-- <?php wp_head(); ?> -->
+</head>
+<body>
+	Тестовая "чистая тема" создана
+</body>
+</html>
