2075ab76c5f1bf199e268bf6e270f2165111b266
yellowcoma <gthtcnfym12git>
2017-02-21 17:54:11 +0300

    header with bloginfo()

--- wp-content/themes/test/index.php
+++ wp-content/themes/test/index.php
@@ -6,11 +6,12 @@
 	<meta name="viewport" content="width=device-width">
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 	<!--[if lt IE 9]>
-	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
+	<script src="<?php echo esc_url(get_template_directory_uri()); ?>/js/html5.js"></script>
 	<![endif]-->
 	<!-- <?php wp_head(); ?> -->
 </head>
 <body>
+	<h1><a href="<?php echo esc_url(bloginfo('url')); ?>"><?php bloginfo('name'); ?></a></h1>
 	Тестовая "чистая тема" создана
 </body>
 </html>
