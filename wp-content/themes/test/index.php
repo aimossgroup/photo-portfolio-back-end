b114f878037854f5884096a39deaf980a5881ba0
yellowcoma <gthtcnfym12git>
2017-03-01 19:21:16 +0300

    add simple cycle

--- wp-content/themes/test/index.php
+++ wp-content/themes/test/index.php
@@ -11,10 +11,23 @@
 	<?php wp_head(); ?>
 </head>
 <body>
+
 <div id="header">
 	<h1><a href="<?php echo esc_url(bloginfo('url')); ?>"><?php bloginfo('name'); ?></a></h1> <!-- возможно здесь esc_url() не нужна -->
 	<p><?php bloginfo('description') ?></p>
 </div>
+
+<!-- Начало цикла выводящего посты -->
+<div class="container">
+<?php if(have_posts()): ?> <!-- проверяет есть ли посты -->
+	<?php while(have_posts()): ?>  <!-- выполняет цикл пока есть ли посты -->
+		<?php the_post(); ?> <!-- Получает запись для вывода. Устанавливает индексы поста в Цикле WP. Переопределяет глобальную переменную $post -->
+		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2> <!-- Отображает заголовок текущего поста -->
+		<?php the_content('читать полностью статью целиком...'); ?> <!-- Отображает содержимое текущего поста -->
+	<?php endwhile; ?>
+<?php endif; ?>
+</div>
+<!--  Конец цикла выводящего посты -->
 
 </body>
 </html>
