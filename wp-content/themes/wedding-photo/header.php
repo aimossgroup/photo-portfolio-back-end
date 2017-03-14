832246c692e9903322a4188f039bea0d4ce39c07
yellowcoma <gthtcnfym12git>
2017-03-14 15:15:56 +0300

    html logo tags to wordpress logo tags

--- wp-content/themes/wedding-photo/header.php
+++ wp-content/themes/wedding-photo/header.php
@@ -22,6 +22,8 @@
     <?php wp_head();  ?>
 </head>
 <body>
+
+
 		<!-- NAV BAR 
 	===================================================-->
 	<nav class="navbar navbar-default navbar-fixed-top">
@@ -34,7 +36,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="index.html">Александр Волков</a>
+				<a class="navbar-brand" href="<?php bloginfo('url') ?>"><?php bloginfo('name') ?></a>
 	    	</div>
 
 			<!-- Collect the nav links, forms, and other content for toggling -->
