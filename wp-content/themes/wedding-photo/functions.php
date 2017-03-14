fd32b94a199eb4f6b9e827699f152dcd3ae8700e
yellowcoma <gthtcnfym12git>
2017-03-14 15:03:37 +0300

    connect wp_bootstrap_navwalker.php to theme

--- wp-content/themes/wedding-photo/functions.php
+++ wp-content/themes/wedding-photo/functions.php
@@ -3,10 +3,14 @@
 /* Включает поддержку темой меню навигации ( в админке появляется соответствующая опция ) */
 add_action( 'after_setup_theme', 'wpt_setup' );
 if ( ! function_exists( 'wpt_setup' ) ):
-	function wpt_setup() {  
+	function wpt_setup() {
 		register_nav_menu( 'primary', __( 'Primary navigation', 'wptuts' ) );
 	}
 endif;
+
+
+/* Подключаем файл wp_bootstrap_navwalker.php в тему */
+require_once('wp_bootstrap_navwalker.php');
 
 
 
