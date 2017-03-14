3e9788788cfeae7f0c42bd7a53c7f28b89f68feb
yellowcoma <gthtcnfym12git>
2017-03-14 16:00:18 +0300

    style update

--- wp-content/themes/wedding-photo/header.php
+++ wp-content/themes/wedding-photo/header.php
@@ -31,7 +31,7 @@
 			<!-- Brand and toggle get grouped for better mobile display -->
 			<div class="navbar-header">
 				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-					<span class="sr-only">Toggle navigation</span>
+					<span class="sr-only">Включить навигацию</span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
@@ -41,20 +41,16 @@
 
 			<!-- Collect the nav links, forms, and other content for toggling -->
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav">
-					<li class="active"><a href="#">Главная <span class="sr-only">(current)</span></a></li>
-					<li><a href="portfolio.html">Портфолио</a></li>
-					<li><a href="prices.html">Услуги и Цены</a></li>
-					<li><a href="testimonials.html">Отзывы клиентов</a></li>
-					<li><a href="contacts.html">Контакты</a></li>
-					<li>
-						<ul class="social_nav">
-							<li><a href=""><i class="fa fa-skype"></i></a></li>
-							<li><a href="http://vk.com" target="_blank"><i class="fa fa-vk"></i></a></li>
-							<li><a href=""><i class="fa fa-whatsapp"></i></a></li>
-						</ul>				
-					</li>        
-				</ul>
+				<?php /* Primary navigation */
+					wp_nav_menu( array(
+						'menu' => 'top_menu',
+						'depth' => 2,
+						'container' => false,
+						'menu_class' => 'nav',
+						//Process nav menu using our custom nav walker
+						'walker' => new wp_bootstrap_navwalker())
+					);
+				 ?>
 			</div><!-- /.navbar-collapse -->
 		</div><!-- /.container-fluid -->
 	</nav>
