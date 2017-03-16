a61d0a85c93311fccac83c23c87181ca5a9d7b42
yellowcoma <gthtcnfym12git>
2017-03-16 15:50:19 +0300

    social icons add

--- wp-content/themes/wedding-photo/functions.php
+++ wp-content/themes/wedding-photo/functions.php
@@ -11,12 +11,18 @@ endif;
 /* Подключаем файл wp_bootstrap_navwalker.php в тему */
 require_once('wp_bootstrap_navwalker.php');
 
-
-	add_filter( 'wp_nav_menu_items', 'your_custom_menu_item', 10, 2 );
-function your_custom_menu_item ( $items, $args ) {
-        $items .= '<li>Show whatever</li>';
-
-    return $items;
+/* Добавляем иконки соц.сетей после генерации меню */
+add_filter( 'wp_nav_menu_items', 'social_icons_add', 10, 2 );
+function social_icons_add ( $items, $args ) {
+        $items .= '
+		<li>
+			<ul class="social_nav">
+				<li><a href=""><i class="fa fa-skype"></i></a></li>
+				<li><a href="http://vk.com" target="_blank"><i class="fa fa-vk"></i></a></li>
+				<li><a href=""><i class="fa fa-whatsapp"></i></a></li>
+			</ul>
+		</li>';
+	return $items;
 }
 
 ?>
