14adbbd4542ea53c850267685d220e292a6d2fe1
yellowcoma <gthtcnfym12git>
2017-03-21 14:07:38 +0300

    code style 21.03-1

--- wp-content/themes/wedding-photo/functions.php
+++ wp-content/themes/wedding-photo/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-/* Включает поддержку темой меню навигации ( в админке появляется соответствующая опция ) */
+// Включает поддержку темой меню навигации ( в админке появляется соответствующая опция ) 
 add_action( 'after_setup_theme', 'wpt_setup' );
 if ( ! function_exists( 'wpt_setup' ) ):
 	function wpt_setup() {
@@ -8,10 +8,10 @@ if ( ! function_exists( 'wpt_setup' ) ):
 	}
 endif;
 
-/* Подключаем файл wp_bootstrap_navwalker.php в тему для подключения меню бутстрапа к WordPress*/
+// Подключаем файл wp_bootstrap_navwalker.php в тему для подключения меню бутстрапа к WordPress
 require_once('wp_bootstrap_navwalker.php');
 
-/* Добавляем иконки соц.сетей после генерации меню */
+// Добавляем иконки соц.сетей после генерации меню 
 add_filter( 'wp_nav_menu_items', 'social_icons_add', 10, 2 );
 function social_icons_add ( $items, $args ) {
         $items .= '
