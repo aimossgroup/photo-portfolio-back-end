088295a535a2a1a04099423a2bdc3956800bd51f
yellowcoma <gthtcnfym12git>
2017-04-05 09:56:25 +0300

    del wedding foto

--- wp-content/themes/wedding-photo/js/navbar_transform.js
+++
@@ -1,8 +0,0 @@
-/* NAVBAR transformation on scroll */
-$(window).scroll(function() {
-	if ($(this).scrollTop() > 200){ // Set position from top to add class
-	$('.navbar').addClass("shrink");
-	} else {
-	$('.navbar').removeClass("shrink");
-	}
-});
